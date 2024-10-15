28864bd24920b76d9b8eba2a803799cefc03bb07
vinaysu <suddakuntivinay7877@gmail.com>
2024-10-15 11:35:01 +0530

    removed the save changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -162,7 +162,7 @@ function Footer() {
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+             
             </div>
           </div>
         </div>
