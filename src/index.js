19e1c70a49c0c194a65ced02ad8edaa02a64a486
vinaysu <suddakuntivinay7877@gmail.com>
2024-10-19 17:19:31 +0530

    hash router with static

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
