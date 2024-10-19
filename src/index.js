9d9d885cb9419eedd7e551e3de052859f978d65d
vinaysu <suddakuntivinay7877@gmail.com>
2024-10-19 16:57:54 +0530

    static.json

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
